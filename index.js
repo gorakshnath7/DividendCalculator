9b9978c4b39e1eaf0e427d3225fb164b3037a31f
Igor <Mascontris@yahoo.com>
2019-06-14 19:34:26 -0600

    css styling

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 'use strict';
 
-// put your own value below!
+// Params for API calls
 const apiKey = 'KQPRIJC81P3ESJF4'; 
 const searchURL = 'https://www.alphavantage.co/query';
+const tickerApiFunction = 'SYMBOL_SEARCH';
+const tickerSymbol = '1. symbol';
+const tickerSeries = 'bestMatches';
+const tickerName = '2. name';
 const meta = 'Meta Data';
 const apiFunction = 'TIME_SERIES_MONTHLY_ADJUSTED';
 const series = 'Monthly Adjusted Time Series';
@@ -38,54 +42,65 @@ function currentDiv(apiSeries){
 //Display results of fetch and calculations
 function displayResults(responseJson, investAmount) {
   // if there are previous results, remove them
-  $('#results-list').empty();
-  $('#js-error-message').empty();
-
-const divPrice = currentDiv(responseJson[series])
-
-let tickerSymbol = `${responseJson[meta][symbol]}`
-let tickerclosePrice = `${parseFloat(divPrice[closePrice]).toFixed(2)}`
-let dividendPrice = `${divPrice[dividendAmount]}`
-let shares = `${(investAmount/divPrice[closePrice]).toFixed(2)}`
-let quarterlyReturn = `${parseFloat(shares*dividendPrice).toFixed(2)}`
-let annualReturn = `${parseFloat(quarterlyReturn*4).toFixed(2)}`
-
-//currentDiv(dividendPrice);
+    $('#results-list').empty();
+    $('#js-error-message').empty();
+    const divPrice = currentDiv(responseJson[series])
+    let tickerSymbol = `${responseJson[meta][symbol]}`
+    let tickerclosePrice = `${parseFloat(divPrice[closePrice]).toFixed(2)}`
+    let dividendPrice = `${divPrice[dividendAmount]}`
+    let shares = `${(investAmount/divPrice[closePrice]).toFixed(2)}`
+    let quarterlyReturn = `${parseFloat(shares*dividendPrice).toFixed(2)}`
+    let annualReturn = `${parseFloat(quarterlyReturn*4).toFixed(2)}`
 
   // generate HTML list with variables
     $('#results-list').append(
       `<li><h3 class='upperCase'>Ticker Symbol: ${tickerSymbol}</h3>
        <li><h3 class='upperCase'>Price per share: $${tickerclosePrice}</h3>
        <li><h3 class='upperCase'>Current Dividend: $${dividendPrice}</h3>
-       <li><h3 class='upperCase'>How many shares you can afford: ${shares}</h3>
+       <li><h3 class='upperCase'>Number of shares: ${shares}</h3>
        <li><h3 class='upperCase'>Quarterly return: $${quarterlyReturn}</h3>
        <li><h3 class='upperCase'>Annual return: $${annualReturn}</h3>
        </li>`     
     )
-};
-
-  //display the results section  
+      //display the results section  
   $('#results').removeClass('hidden');
+}
 
-  //API fetch function using params
-function getStockResults(query, investAmount) {
-  const params = {
-    apikey: apiKey,
-    function: apiFunction,
-    symbol: query
-  };
-  const queryString = formatQueryParams(params)
-  const url = searchURL + '?' + queryString;
+/* Need paid API 
+function displaySearchList(responseJson) {
+    $('#search-list').empty();
+    $('#search-list').append(
+        `<li>${responseJson[tickerSeries][0][tickerSymbol]}               ${responseJson[tickerSeries][0][tickerName]}
+         <li>${responseJson[tickerSeries][1][tickerSymbol]}               ${responseJson[tickerSeries][1][tickerName]}
+         <li>${responseJson[tickerSeries][2][tickerSymbol]}               ${responseJson[tickerSeries][2][tickerName]}
+         <li>${responseJson[tickerSeries][3][tickerSymbol]}               ${responseJson[tickerSeries][3][tickerName]} 
+         <li>${responseJson[tickerSeries][4][tickerSymbol]}               ${responseJson[tickerSeries][4][tickerName]}</li>`
+    )
+}*/
 
-  console.log(url);
+//send fetch request to API with params
+function queryAPI(params) {
+    params.apikey = apiKey
+    const queryString = formatQueryParams(params)
+    const url = searchURL + '?' + queryString;
 
-  fetch(url)
+    return fetch(url)
     .then(response => {
       if (response.ok) {
         return response.json();
       }
       throw new Error(response.statusText);
     })
+}
+
+  //API fetch for stock div information
+function getStockResults(query, investAmount) {
+  const params = {
+    function: apiFunction,
+    symbol: query
+  };
+
+    queryAPI(params)
     .then(responseJson => displayResults(responseJson, investAmount))
     .catch(err => {
         console.log(err)
@@ -95,10 +110,29 @@ function getStockResults(query, investAmount) {
 
         else if (err.message == `Cannot convert undefined or null to object`) {
             err.message = `This is not a valid stock ticker.`}
-
-      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+      
+      $('#results').addClass('hidden')      
+      $('#js-error-message').text(`${err.message}`);
     });
 }
+
+/* Need paid API 
+//API fetch for ticker search
+function getTickerSymbol(inputQuery){
+    const params = {
+        function: tickerApiFunction,
+        keywords: inputQuery,
+    }
+    queryAPI(params)
+    .then(responseJson => displaySearchList(responseJson))
+    .catch(err => {
+        $('#js-error-message').empty();
+        console.log(err)
+        console.log(err.message)
+        $('#js-error-message').text('This is not a valid stock ticker.')
+    });
+}
+*/
 
 //Submit listener for ticker symbol and investment amount
 function watchForm() {
@@ -110,5 +144,20 @@ function watchForm() {
   });
 }
 
+/* Need paid API 
+//listener for ticker symbol field to populate text
+function tickerSymbolChange() {
+    $('#js-search-term').on('input', event => {
+        const tickerTerm = $('#js-search-term').val();
+        getTickerSymbol(tickerTerm);
+    });
+}
+*/
+
+function initializeUI() {
+    watchForm()
+    //tickerSymbolChange()
+}
+
 //run submit listener
-$(watchForm);
+$(initializeUI);
