f2c040ef21915c77c5a6515f5f8d2b81671054a0
Igor <Mascontris@yahoo.com>
2019-06-15 00:52:07 -0600

    animation

--- index.js
+++ index.js
@@ -54,12 +54,18 @@ function displayResults(responseJson, investAmount) {
 
   // generate HTML list with variables
     $('#results-list').append(
-      `<li><h3 class='upperCase'>Ticker Symbol: ${tickerSymbol}</h3>
-       <li><h3 class='upperCase'>Price per share: $${tickerclosePrice}</h3>
-       <li><h3 class='upperCase'>Current Dividend: $${dividendPrice}</h3>
-       <li><h3 class='upperCase'>Number of shares: ${shares}</h3>
-       <li><h3 class='upperCase'>Quarterly return: $${quarterlyReturn}</h3>
-       <li><h3 class='upperCase'>Annual return: $${annualReturn}</h3>
+      `<li>Ticker Symbol:
+       <li class='alignRight slide-left'>${tickerSymbol}
+       <li class='slide-right'>Price per share:
+       <li class='alignRight slide-left'>$${tickerclosePrice}
+       <li class='slide-right'>Current Dividend: 
+       <li class='alignRight slide-left'>$${dividendPrice}
+       <li class='slide-right'>Number of shares: 
+       <li class='alignRight slide-left'>${shares}
+       <li class='slide-right'>Quarterly return: 
+       <li class='alignRight slide-left'>$${quarterlyReturn}
+       <li class='slide-right'>Annual return: 
+       <li class='alignRight slide-left'>$${annualReturn}
        </li>`     
     )
       //display the results section  
@@ -139,7 +145,9 @@ function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
     const searchTerm = $('#js-search-term').val();
-    const investAmount = $('#js-max-results').val();
+    let investAmount = $('#js-max-results').val();
+    investAmount = parseFloat(investAmount.replace(/\$|,/g, ''))
+    console.log(investAmount)
     getStockResults(searchTerm, investAmount);
   });
 }
@@ -153,6 +161,90 @@ function tickerSymbolChange() {
     });
 }
 */
+
+//This is the currency modifier
+$("input[data-type='currency']").on({
+  keyup: function() {
+    formatCurrency($(this));
+  },
+  blur: function() { 
+    formatCurrency($(this), "blur");
+  }
+});
+
+
+function formatNumber(n) {
+// format number 1000000 to 1,234,567
+return n.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+}
+
+
+function formatCurrency(input, blur) {
+// appends $ to value, validates decimal side
+// and puts cursor back in right position.
+
+// get input value
+var input_val = input.val();
+
+// don't validate empty input
+if (input_val === "") { return; }
+
+// original length
+var original_len = input_val.length;
+
+// initial caret position 
+var caret_pos = input.prop("selectionStart");
+  
+// check for decimal
+if (input_val.indexOf(".") >= 0) {
+
+  // get position of first decimal
+  // this prevents multiple decimals from
+  // being entered
+  var decimal_pos = input_val.indexOf(".");
+
+  // split number by decimal point
+  var left_side = input_val.substring(0, decimal_pos);
+  var right_side = input_val.substring(decimal_pos);
+
+  // add commas to left side of number
+  left_side = formatNumber(left_side);
+
+  // validate right side
+  right_side = formatNumber(right_side);
+  
+  // On blur make sure 2 numbers after decimal
+  if (blur === "blur") {
+    right_side += "00";
+  }
+  
+  // Limit decimal to only 2 digits
+  right_side = right_side.substring(0, 2);
+
+  // join number by .
+  input_val = "$" + left_side + "." + right_side;
+
+} else {
+  // no decimal entered
+  // add commas to number
+  // remove all non-digits
+  input_val = formatNumber(input_val);
+  input_val = "$" + input_val;
+  
+  // final formatting
+  if (blur === "blur") {
+    input_val += ".00";
+  }
+}
+
+// send updated string to input
+input.val(input_val);
+
+// put caret back in the right position
+var updated_len = input_val.length;
+caret_pos = updated_len - original_len + caret_pos;
+input[0].setSelectionRange(caret_pos, caret_pos);
+}
 
 function initializeUI() {
     watchForm()
